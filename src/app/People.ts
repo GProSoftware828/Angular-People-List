a870f8ed55bc31d096e83920c4f1544996380592
GproSoftware828 <JetSendProductions@Gmail.com>
2019-11-05 00:11:50 +1100

    Added table with people model object and selector

--- src/app/People.ts
+++ src/app/People.ts
@@ -2,7 +2,17 @@ export class People {
   Id: number;
   Name: string;
   Salary: number;
-  constructor(id: number, name: string, salary: number) {
-    (this.Id = id), (this.Name = name), (this.Salary = salary);
+  Type: PeopleType;
+  constructor(id: number, name: string, salary: number, type: PeopleType) {
+    (this.Id = id),
+      (this.Name = name),
+      (this.Salary = salary),
+      (this.Type = type);
   }
+}
+
+export enum PeopleType {
+  Temporary,
+  Permanent,
+  Contract
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { People } from './People';
+import { People, PeopleType } from './People';
 
 @Component({
   selector: 'app-root',
@@ -10,12 +10,13 @@ export class AppComponent {
   PeopleListSelected: boolean = true;
   Person1: string = 'User One';
   PeopleList: People[] = [
-    new People(1, 'Geo', 10450),
-    new People(2, 'Ryan', 10030),
-    new People(3, 'Jenny', 10001),
-    new People(4, 'Hannah', 10070),
-    new People(5, 'Josh', 10002)
+    new People(1, 'Geo', 10450, PeopleType.Permanent),
+    new People(2, 'Ryan', 10030, PeopleType.Contract),
+    new People(3, 'Jenny', 10001, PeopleType.Temporary),
+    new People(4, 'Hannah', 10070, PeopleType.Permanent),
+    new People(5, 'Josh', 10002, PeopleType.Temporary)
   ];
+  SelectedPeopleList: People = this.PeopleList[2];
   Person2: string = 'User Two';
   Duration: number = 7;
   PeopleCategories: string[] = [
