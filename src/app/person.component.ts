a5659684a6459a4418113422eebcf0b2f10c33ca
GproSoftware828 <JetSendProductions@Gmail.com>
2019-11-05 13:58:01 +1100

    Added 2-way binding

--- src/app/person.component.ts
+++ src/app/person.component.ts
@@ -5,9 +5,21 @@ import { Person } from './Person';
   selector: 'person-component',
   template: `
     <form>
-      First Name: <input type="text" [value]="person.firstName" /><br />
-      Last Name: <input type="text" [value]="person.lastName" /><br />
+      First Name:
+      <input
+        type="text"
+        [value]="person.firstName"
+        (input)="person.firstName = $event.target.value"
+      /><br />
+      Last Name:
+      <input
+        type="text"
+        [value]="person.lastName"
+        #lastName
+        (input)="person.lastName = lastName.value"
+      /><br />
     </form>
+    Hello {{ person.firstName + ' ' + person.lastName }}
   `
 })
 export class PersonComponent {
