2ee95c78fe27ff0f385ae577f2b1b4f4e0c7e133
GproSoftware828 <JetSendProductions@Gmail.com>
2019-11-06 21:52:17 +1100

    Added rxjs debounce and distinct

--- src/app/person.form.component.ts
+++ src/app/person.form.component.ts
@@ -6,6 +6,8 @@ import {
   FormBuilder
 } from '@angular/forms';
 import { personForm } from './form-person';
+import { Observable, fromEvent } from 'rxjs';
+import { debounceTime, map, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'person-form',
@@ -15,6 +17,7 @@ export class FormComponent implements OnInit {
   constructor(private fb: FormBuilder) {}
 
   person: personForm;
+  changes: string[] = [];
   firstName: FormControl;
   lastName: FormControl;
   personForm: FormGroup;
@@ -30,21 +33,21 @@ export class FormComponent implements OnInit {
     this.person.lastName = 'Blow';
     this.person.gender = 'female';
     this.person.qualification = 'Certifications';
-    // this.createFormControls();
-    // this.createForm();
+    this.createFormControls();
+    this.createForm();
 
-    this.personForm = this.fb.group({
-      firstName: [
-        'Joe',
-        [Validators.required, Validators.pattern('[a-zA-Z0-9]+')]
-      ],
-      lastName: [
-        'Bloe',
-        [Validators.required, Validators.pattern('[a-zA-Z0-9]+')]
-      ],
-      gender: ['male'],
-      qualification: ['Certifications']
-    });
+    // this.personForm = this.fb.group({
+    //   firstName: [
+    //     'Joe',
+    //     [Validators.required, Validators.pattern('[a-zA-Z0-9]+')]
+    //   ],
+    //   lastName: [
+    //     'Bloe',
+    //     [Validators.required, Validators.pattern('[a-zA-Z0-9]+')]
+    //   ],
+    //   gender: ['male'],
+    //   qualification: ['Certifications']
+    // });
 
     // this.personForm.get('firstName'.valueChanges.debounceTime(500).distinctUntilChanged().
     //   this.changes.push(change);
@@ -57,27 +60,38 @@ export class FormComponent implements OnInit {
     });
   }
 
-  // createFormControls() {
-  //   this.firstName = new FormControl('Joe', [
-  //     Validators.required,
-  //     Validators.pattern('[a-zA-Z0-9 ]+')
-  //   ]);
-  //   this.lastName = new FormControl('Blow', [
-  //     Validators.required,
-  //     Validators.pattern('[a-zA-Z0-9 ]+')
-  //   ]);
-  //   this.gender = new FormControl('male');
-  //   this.qualification = new FormControl('Certifications');
-  // }
+  createFormControls() {
+    this.firstName = new FormControl('Joe', [
+      Validators.required,
+      Validators.pattern('[a-zA-Z0-9 ]+')
+    ]);
+    const output = document.getElementById('firstName');
+    const keyup$ = fromEvent(output, 'keyup');
+    keyup$
+      .pipe(
+        map((i: any) => i.currentTarget.value),
+        debounceTime(500),
+        distinctUntilChanged()
+      )
+      .subscribe(change => {
+        this.changes.push(change);
+      });
+    this.lastName = new FormControl('Blow', [
+      Validators.required,
+      Validators.pattern('[a-zA-Z0-9 ]+')
+    ]);
+    this.gender = new FormControl('male');
+    this.qualification = new FormControl('Certifications');
+  }
 
-  // createForm() {
-  //   this.personForm = new FormGroup({
-  //     firstName: this.firstName,
-  //     lastName: this.lastName,
-  //     gender: this.gender,
-  //     qualification: this.qualification
-  //   });
-  // }
+  createForm() {
+    this.personForm = new FormGroup({
+      firstName: this.firstName,
+      lastName: this.lastName,
+      gender: this.gender,
+      qualification: this.qualification
+    });
+  }
 
   onSubmit(form: any) {
     if (this.personForm.valid) {
